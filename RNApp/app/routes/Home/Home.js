f6344f5f93c378861e7bab3b1926b2e70a8a4288
Mark <qdmark@gmail.com>
2017-07-13 14:52:14 -0700

    align the buttons

--- RNApp/app/routes/Home/Home.js
+++ RNApp/app/routes/Home/Home.js
@@ -6,20 +6,26 @@ import styles from './styles';
 const Home = (props) => {
   return (
     <View style={styles.container}>
-      <View style={{width: 300, height: 300, margin: 30, backgroundColor: 'lightgrey'}} />
-      <Button
-        style={{width: 30}}
-        text="Feed"
-        onPress={props.onDetailsPress}
-      />
-      <Button
-        text="Clearn"
-        onPress={props.onDetailsPress}
-      />
-      <Button
-        text="Details"
-        onPress={props.onDetailsPress}
-      />
+      <View style={{width: 300, height: 300, margin: 10, marginTop: 120, backgroundColor: 'lightgrey'}} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      }}>
+        <Button
+          text="Feed"
+          onPress={props.onDetailsPress}
+        />
+        <Button
+          text="Clearn"
+          onPress={props.onDetailsPress}
+        />
+        <Button
+          text="Play"
+          onPress={props.onDetailsPress}
+        />
+      </View>
     </View>
   );
 };
