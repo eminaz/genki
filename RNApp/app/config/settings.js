be54c445bd6bb24d3561b79b87a6c87070990795
hinling yeung <hinling.yeung@ihealthlabs.com>
2017-07-14 13:07:48 -0700

    add xcode project settings

--- RNApp/app/config/settings.js
+++ RNApp/app/config/settings.js
@@ -1,5 +1,7 @@
 // If you're running on a device or in the Android simulator be sure to change
-let METEOR_URL = 'ws://localhost:3000/websocket';
+//let meteor_host = process.env.METEOR_HOST || 'localhost';
+console.log("=========> you will need to change the METOER_HOST to your own ip or url");
+let METEOR_URL = 'ws://192.168.120.10:3000/websocket';
 if (process.env.NODE_ENV === 'production') {
   METEOR_URL = ''; // your production server url
 }
