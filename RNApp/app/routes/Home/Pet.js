d917fc54ef01f3cbb7b796153a9073fed2b524c7
Mark <qdmark@gmail.com>
2017-07-14 12:00:13 -0700

    test adding camera

--- RNApp/app/routes/Home/Pet.js
+++ RNApp/app/routes/Home/Pet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 import images from './images';
+import Camera from 'react-native-camera';
 
 const getImageSource = (pet) => {
   if (pet.eating) {
@@ -14,23 +15,41 @@ const getImageSource = (pet) => {
   }
 }
 
-const Component = (props) => {
-  return (
-    <View style={{
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <Image
-        style={{
-          margin: 0, marginTop: 100, backgroundColor: 'lightgrey',
-        }}
-        source={getImageSource(props.pet)}
-        defaultSource={images.idle}
-        maxWidth={300}
-        maxHeight={400}
-      />
-    </View>
-  );
+const Component = class extends React.Component {
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <View style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            aspect={Camera.constants.Aspect.fill}>
+            <Text onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+
+        <Image
+          style={{
+            margin: 0, marginTop: 100, backgroundColor: 'lightgrey',
+          }}
+          source={getImageSource(this.props.pet)}
+          defaultSource={images.idle}
+          maxWidth={300}
+          maxHeight={400}
+        />
+      </View>
+    );
+  }
 }
 
 export default Component;
